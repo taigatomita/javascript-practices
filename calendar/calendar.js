fe0039a87b20585fd8f08c6466b3390c3d7b5a45
taigatomita <tiger101837@gmail.com>
2021-12-22 05:00:37 +0900

    変数名を改名

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -4,18 +4,18 @@ const argv = require('minimist')(process.argv.slice(2))
 const date = argv.y && argv.m ? dayjs(new Date(argv.y, argv.m - 1)) : dayjs()
 const BeginningOfMonth = date.set('date', 1)
 
-const saturday = DateNum => {
-  return (date.set('date', DateNum).day() === 6)
+const saturday = DateNumber => {
+  return (date.set('date', DateNumber).day() === 6)
 }
 
 console.log(`      ${date.get('month') + 1}月 ${date.get('year')}`)
 console.log('日 月 火 水 木 金 土')
 
-for (let i = 0; BeginningOfMonth.day() > i; i++) {
+for (let count = 0; BeginningOfMonth.day() > count; count++) {
   process.stdout.write('   ')
 }
-for (let DateNum = 1; date.daysInMonth() >= DateNum; DateNum++) {
-  process.stdout.write(DateNum.toString().padStart(2, ' '))
-  process.stdout.write(saturday(DateNum) ? '\n' : ' ')
+for (let DateNumber = 1; date.daysInMonth() >= DateNumber; DateNumber++) {
+  process.stdout.write(DateNumber.toString().padStart(2, ' '))
+  process.stdout.write(saturday(DateNumber) ? '\n' : ' ')
 }
 process.stdout.write('\n')
