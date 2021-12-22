4353d9f8c72583a743005964a93eb68e41a7f19d
taigatomita <tiger101837@gmail.com>
2021-12-22 16:23:40 +0900

    変数,メソッドを改名

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -2,20 +2,20 @@ const dayjs = require('dayjs')
 const argv = require('minimist')(process.argv.slice(2))
 
 const date = argv.y && argv.m ? dayjs(new Date(argv.y, argv.m - 1)) : dayjs()
-const BeginningOfMonth = date.set('date', 1)
+const beginningOfMonth = date.set('date', 1)
 
-const saturday = DateNumber => {
-  return (date.set('date', DateNumber).day() === 6)
+const isSaturday = dateNumber => {
+  return (date.set('date', dateNumber).day() === 6)
 }
 
 console.log(`      ${date.get('month') + 1}月 ${date.get('year')}`)
 console.log('日 月 火 水 木 金 土')
 
-for (let count = 0; BeginningOfMonth.day() > count; count++) {
+for (let count = 0; beginningOfMonth.day() > count; count++) {
   process.stdout.write('   ')
 }
-for (let DateNumber = 1; date.daysInMonth() >= DateNumber; DateNumber++) {
-  process.stdout.write(DateNumber.toString().padStart(2, ' '))
-  process.stdout.write(saturday(DateNumber) ? '\n' : ' ')
+for (let dateNumber = 1; date.daysInMonth() >= dateNumber; dateNumber++) {
+  process.stdout.write(dateNumber.toString().padStart(2, ' '))
+  process.stdout.write(isSaturday(dateNumber) ? '\n' : ' ')
 }
 process.stdout.write('\n')
